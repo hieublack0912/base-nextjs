9d01da41e29aaeeaf97ae336a333285045fa3483
hieublack0912 <hieublack0912@gmail.com>
2022-12-21 15:35:18 +0700

    Base tailwind

---
+++ src/components/Demo/Demo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Demo: React.FC = () => {
+    return (
+        <div className={"text-lg"}>
+            test TW
+        </div>
+    )
+}
+
+export default Demo;
